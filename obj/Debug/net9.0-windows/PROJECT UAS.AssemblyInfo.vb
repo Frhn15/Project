7bc0dfb161acfb03b5862a2f5fbcf937de587678
user <barengbelajar143@gmail.com>
2025-12-09 22:25:03 +0700

    ubah desain form transaksi masuk (UI)

--- obj/Debug/net9.0-windows/PROJECT UAS.AssemblyInfo.vb
+++ obj/Debug/net9.0-windows/PROJECT UAS.AssemblyInfo.vb
@@ -16,7 +16,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("PROJECT UAS"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Debug"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00875f42b2334ddfb480316380bd2cffdfd96f59"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e2787c9832e93fd52552eb8f43a01357c086f32"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("PROJECT UAS"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("PROJECT UAS"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0"),  _
