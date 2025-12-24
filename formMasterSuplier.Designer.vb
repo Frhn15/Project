e8621eaf73c0591113e1d29d8bc468d615dcda16
nazwa <nazwawadestiamaharani@gmail.com>
2025-12-24 18:19:41 +0700

    testtt

--- formMasterSuplier.Designer.vb
+++ formMasterSuplier.Designer.vb
@@ -22,15 +22,175 @@ Partial Class formMasterSuplier
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        TextBox1 = New TextBox()
+        Button1 = New Button()
+        MaskedTextBox1 = New MaskedTextBox()
+        TextBox2 = New TextBox()
+        Label3 = New Label()
+        TextBox3 = New TextBox()
+        Label4 = New Label()
+        Label5 = New Label()
+        Button2 = New Button()
+        Button3 = New Button()
+        PictureBox1 = New PictureBox()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 16F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(641, 77)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(303, 45)
+        Label1.TabIndex = 18
+        Label1.Text = "MASTER SUPPLIER"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(459, 207)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(63, 25)
+        Label2.TabIndex = 19
+        Label2.Text = "Label2"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(655, 201)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(570, 31)
+        TextBox1.TabIndex = 20
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(655, 652)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(112, 34)
+        Button1.TabIndex = 21
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' MaskedTextBox1
+        ' 
+        MaskedTextBox1.Location = New Point(655, 539)
+        MaskedTextBox1.Name = "MaskedTextBox1"
+        MaskedTextBox1.Size = New Size(570, 31)
+        MaskedTextBox1.TabIndex = 22
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(655, 311)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(570, 31)
+        TextBox2.TabIndex = 24
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(459, 317)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(63, 25)
+        Label3.TabIndex = 23
+        Label3.Text = "Label3"
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(655, 427)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(570, 31)
+        TextBox3.TabIndex = 26
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(459, 433)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(63, 25)
+        Label4.TabIndex = 25
+        Label4.Text = "Label4"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(459, 545)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(63, 25)
+        Label5.TabIndex = 27
+        Label5.Text = "Label5"
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(1113, 652)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(112, 34)
+        Button2.TabIndex = 28
+        Button2.Text = "Button2"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(888, 652)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(112, 34)
+        Button3.TabIndex = 29
+        Button3.Text = "Button3"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.BackColor = Color.Transparent
+        PictureBox1.BackgroundImage = My.Resources.Resources.Logo_Kelompok_3_Sistem_Inventory
+        PictureBox1.BackgroundImageLayout = ImageLayout.Zoom
+        PictureBox1.Dock = DockStyle.Bottom
+        PictureBox1.Location = New Point(0, 687)
+        PictureBox1.Margin = New Padding(4)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(1578, 154)
+        PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox1.TabIndex = 30
+        PictureBox1.TabStop = False
         ' 
         ' formMasterSuplier
         ' 
-        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleDimensions = New SizeF(10F, 25F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(800, 450)
+        BackgroundImage = My.Resources.Resources.assd
+        BackgroundImageLayout = ImageLayout.Stretch
+        ClientSize = New Size(1578, 841)
+        Controls.Add(PictureBox1)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Label5)
+        Controls.Add(TextBox3)
+        Controls.Add(Label4)
+        Controls.Add(TextBox2)
+        Controls.Add(Label3)
+        Controls.Add(MaskedTextBox1)
+        Controls.Add(Button1)
+        Controls.Add(TextBox1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Margin = New Padding(4)
         Name = "formMasterSuplier"
         Text = "formMasterSuplier"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
+        PerformLayout()
     End Sub
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents MaskedTextBox1 As MaskedTextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents PictureBox1 As PictureBox
 End Class
